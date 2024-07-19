37ea34484315c5c79f85f75a0d7e6ee4d199a922
MJ <wkdaudwn11@naver.com>
2024-07-19 09:06:20 +0900

    refactor: web push service code

--- src/services/web-push.ts
+++ src/services/web-push.ts
@@ -2,71 +2,54 @@ import { urlBase64ToUint8Array } from '@/utils';
 
 import api from '.';
 
-export const clearSubscribeUser = () => {
-  navigator.serviceWorker.ready.then((registration) => {
-    registration.pushManager.getSubscription().then((subscription) => {
-      if (subscription) {
-        subscription
-          .unsubscribe()
-          .then(() => {
-            api
-              .delete('/api/subscribe')
-              .then(() => {
-                console.log(
-                  'Successfully clear subscribed and notified the server.',
-                );
-              })
-              .catch((error) => {
-                console.error(
-                  'Failed to notify the server about the clear subscription',
-                  error,
-                );
-              });
-          })
-          .catch((error) => {
-            console.error('Clear subscription failed', error);
-          });
-      }
+export const clearSubscribeUser = async () => {
+  const registration: ServiceWorkerRegistration =
+    await navigator.serviceWorker.ready;
+  const subscription: PushSubscription =
+    await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(
+        process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || '',
+      ),
     });
-  });
+
+  if (subscription) {
+    await subscription.unsubscribe();
+    api.delete('/api/subscribe');
+  }
 };
 
-export const subscribeUser = () => {
-  navigator.serviceWorker.ready.then(
-    (registration: ServiceWorkerRegistration) => {
-      registration.pushManager
-        .getSubscription()
-        .then((subscription: PushSubscription | null) => {
-          if (subscription) {
-            console.log('Already subscribed');
-          } else {
-            registration.pushManager
-              .subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: urlBase64ToUint8Array(
-                  process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || '',
-                ),
-              })
-              .then((subscription2: PushSubscription) => {
-                const keyP256dh = subscription2.getKey('p256dh');
-                const keyAuth = subscription2.getKey('auth');
-                api.post('/api/subscribe', {
-                  endpoint: subscription2.endpoint,
-                  expirationTime: subscription2.expirationTime,
-                  keys: {
-                    p256dh: keyP256dh
-                      ? btoa(String.fromCharCode(...new Uint8Array(keyP256dh)))
-                      : '',
-                    auth: keyAuth
-                      ? btoa(String.fromCharCode(...new Uint8Array(keyAuth)))
-                      : '',
-                  },
-                });
-              });
-          }
-        });
+export const subscribeUser = async () => {
+  const registration: ServiceWorkerRegistration =
+    await navigator.serviceWorker.ready;
+
+  const newSubscription: PushSubscription =
+    await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(
+        process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || '',
+      ),
+    });
+
+  const keyP256dh = newSubscription.getKey('p256dh');
+  const keyAuth = newSubscription.getKey('auth');
+
+  const payload = {
+    endpoint: newSubscription.endpoint,
+    expirationTime: newSubscription.expirationTime,
+    keys: {
+      p256dh: keyP256dh
+        ? btoa(String.fromCharCode(...new Uint8Array(keyP256dh)))
+        : '',
+      auth: keyAuth
+        ? btoa(String.fromCharCode(...new Uint8Array(keyAuth)))
+        : '',
     },
-  );
+  };
+
+  await api.post('/api/subscribe', {
+    ...payload,
+  });
 };
 
 export const sendNotification = async () => {
